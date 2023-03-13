60024e7afa27097c1fd229789e3809601714bf8e
Jon Boone <ipmonger@delamancha.org>
2023-03-13 14:38:51 -0400

    adds initial profile configurations

---
+++ profiles.el
@@ -0,0 +1,6 @@
+;; define custom profiles
+(("default" . ((user-emacs-directory . "~/.config/emacs/.emacs.default")))
+ ("spacemacs" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.config/emacs/spacemacs.d")))))
+ ("literate-emacs-config" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config"))))
+
