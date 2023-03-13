5d9a321bad3ff40a11e20e0b3ca5c883f574aebf
Jon Boone <ipmonger@delamancha.org>
2023-03-13 15:01:50 -0400

    makes spacemacs the default working configuration

--- profiles.el
+++ profiles.el
@@ -1,6 +1,4 @@
 ;; define custom profiles
-(("default" . ((user-emacs-directory . "~/.config/emacs/.emacs.default")))
- ("spacemacs" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/.config/emacs/spacemacs.d")))))
+(("default" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
+               (env . (("SPACEMACSDIR" . "~/.config/emacs/spacemacs.d")))))
  ("literate-emacs-config" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config"))))
-
