b3d981e53336879536a4bbeedd65dac03df5fb4a
Jon Boone <ipmonger@delamancha.org>
2023-04-12 09:26:56 -0400

    update profiles

--- profiles.el
+++ profiles.el
@@ -1,4 +1,6 @@
 ;; define custom profiles
 (("default" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
-               (env . (("SPACEMACSDIR" . "~/.config/emacs/spacemacs.d")))))
+               (env . (("SPACEMACSDIR" . "~/.config/emacs/iain-spacemacs-config")))))
+ ("practicali" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
+                  (env . (("SPACEMACSDIR" . "~/.config/emacs/practicalli-spacemacs-config")))))
  ("literate-emacs-config" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config"))))
