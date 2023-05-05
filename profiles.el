f85d6ed49d44543b8c16c3e3a8c426bf30e723c0
Jon Boone <ipmonger@delamancha.org>
2023-05-05 16:58:34 -0400

    added new org config (default) and lisp config

--- profiles.el
+++ profiles.el
@@ -1,5 +1,7 @@
 ;; define custom profiles
-(("default" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
+(("default" . ((user-emacs-directory . "~/.config/emacs/org-config")))
+ ("lisp" . ((user-emacs-directory . "~/.config/emacs/lisp-config")))
+ ("spacemacs" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
                (env . (("SPACEMACSDIR" . "~/.config/emacs/iain-spacemacs-config")))))
  ("practicali" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
                   (env . (("SPACEMACSDIR" . "~/.config/emacs/practicalli-spacemacs-config")))))
