f8722ea64bb3dae4fe015fe081aacedc4ee68653
Jon Boone <ipmonger@delamancha.org>
2023-06-07 11:49:38 -0400

    updating to minimal config

--- profiles.el
+++ profiles.el
@@ -1,8 +1,2 @@
 ;; define custom profiles
-(("default" . ((user-emacs-directory . "~/.config/emacs/org-config")))
- ("lisp" . ((user-emacs-directory . "~/.config/emacs/lisp-config")))
- ("spacemacs" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
-               (env . (("SPACEMACSDIR" . "~/.config/emacs/iain-spacemacs-config")))))
- ("practicali" . ((user-emacs-directory . "~/.config/emacs/spacemacs")
-                  (env . (("SPACEMACSDIR" . "~/.config/emacs/practicalli-spacemacs-config")))))
- ("literate-emacs-config" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config"))))
+(("default" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config"))))
