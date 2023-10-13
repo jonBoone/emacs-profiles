362291e28ec6d1618aa0770f22fdb1d96f4a590b
Jon Boone <ipmonger@delamancha.org>
2023-10-12 23:18:34 -0400

    update default to new compartmentalized version

--- profiles.el
+++ profiles.el
@@ -1,3 +1,4 @@
 ;; define custom profiles
-(("default" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config")))
- ("roswell" . ((user-emacs-directory . "~/.config/emacs/roswell-emacs-config"))))
+(("default"        . ((user-emacs-directory . "~/.config/emacs/compartmentalized-emacs-config")))
+ ("literate-emacs" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config")))
+ ("roswell"        . ((user-emacs-directory . "~/.config/emacs/roswell-emacs-config"))))
