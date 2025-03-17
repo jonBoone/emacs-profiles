7fae7186252f08204d25113ff4b8d41f0cc67345
Jon Boone <ipmonger@delamancha.org>
2025-03-16 22:56:36 -0400

    updating location of spacemacs directory

--- profiles.el
+++ profiles.el
@@ -1,5 +1,5 @@
 ;; define custom profiles
-(("default"        . ((user-emacs-directory . "~/.config/emacs/spacemacs-config")))
+(("default"        . ((user-emacs-directory . "~/.spacemacs.d")))
  ;; ("literate-emacs" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config")))
  ;; ("compartmentalized-emacs" . ((user-emacs-directory . "~/.config/emacs/compartmentalized-emacs-config")))
  ;; ("roswell"        . ((user-emacs-directory . "~/.config/emacs/roswell-emacs-config")))
