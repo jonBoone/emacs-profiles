7e254cb82f145c93db2796b348773cea2c648b00
Jon Boone <ipmonger@delamancha.org>
2025-03-16 22:50:55 -0400

    adjusting formatting

--- profiles.el
+++ profiles.el
@@ -1,5 +1,6 @@
 ;; define custom profiles
-(("default"        . ((user-emacs-directory . "~/.config/emacs/spacemacs-config"))))
+(("default"        . ((user-emacs-directory . "~/.config/emacs/spacemacs-config")))
  ;; ("literate-emacs" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config")))
  ;; ("compartmentalized-emacs" . ((user-emacs-directory . "~/.config/emacs/compartmentalized-emacs-config")))
  ;; ("roswell"        . ((user-emacs-directory . "~/.config/emacs/roswell-emacs-config")))
+ )
