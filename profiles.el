177e348c08128f461c6571eddcb6d73628e17f7b
Jon Boone <ipmonger@delamancha.org>
2025-03-16 22:35:20 -0400

    updating with the actual spacemacs-config repo reference

--- profiles.el
+++ profiles.el
@@ -1,6 +1,5 @@
 ;; define custom profiles
-(("default"        . ((user-emacs-directory . "~/.config/emacs/spacemacs")))
+(("default"        . ((user-emacs-directory . "~/.config/emacs/spacemacs-config"))))
  ;; ("literate-emacs" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config")))
  ;; ("compartmentalized-emacs" . ((user-emacs-directory . "~/.config/emacs/compartmentalized-emacs-config")))
  ;; ("roswell"        . ((user-emacs-directory . "~/.config/emacs/roswell-emacs-config")))
- )
