41cdeaf4fcbb6c0c9722d37f5510d71463e189d5
Jon Boone <ipmonger@delamancha.org>
2025-05-18 03:14:32 -0400

    add scimax profile as default; emacs-solo as secondary (#1)

--- profiles.el
+++ profiles.el
@@ -1,6 +1,8 @@
 ;; define custom profiles
-(("default"        . ((user-emacs-directory . "~/.spacemacs.d")))
+(("default"        . ((user-emacs-directory . "~/.config/emacs/scimax")))
+ ("emacs-solo"     . ((user-emacs-directory . "~/.config/emacs/emacs-solo"))))
+ ;; ("spacemacs"      . ((user-emacs-directory . "~/.config/emacs/spacemacs")
+ ;;                      (env                  . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
  ;; ("literate-emacs" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config")))
  ;; ("compartmentalized-emacs" . ((user-emacs-directory . "~/.config/emacs/compartmentalized-emacs-config")))
  ;; ("roswell"        . ((user-emacs-directory . "~/.config/emacs/roswell-emacs-config")))
- )
