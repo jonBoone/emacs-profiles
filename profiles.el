62e7ee608bdd6206784a52dbe292859d62967e85
Jon Boone <ipmonger@delamancha.org>
2025-06-01 20:27:40 -0400

    adding compartmentalized-emacs-config back as default profile

--- profiles.el
+++ profiles.el
@@ -1,9 +1,9 @@
 ;; define custom profiles
-(("default"        . ((user-emacs-directory . "~/.config/emacs/scimax")
-		      (env                  . (("SCIMAXDIR" . "~/.scimax.d")))))
- ("emacs-solo"     . ((user-emacs-directory . "~/.config/emacs/emacs-solo"))))
+(("scimax"                  . ((user-emacs-directory . "~/.config/emacs/scimax")
+		               (env                  . (("SCIMAXDIR" . "~/.scimax.d")))))
+ ("emacs-solo"              . ((user-emacs-directory . "~/.config/emacs/emacs-solo")))
+ ("default"                 . ((user-emacs-directory . "~/.config/emacs/compartmentalized-emacs-config"))))
  ;; ("spacemacs"      . ((user-emacs-directory . "~/.config/emacs/spacemacs")
  ;;                      (env                  . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
  ;; ("literate-emacs" . ((user-emacs-directory . "~/.config/emacs/literate-emacs-config")))
- ;; ("compartmentalized-emacs" . ((user-emacs-directory . "~/.config/emacs/compartmentalized-emacs-config")))
  ;; ("roswell"        . ((user-emacs-directory . "~/.config/emacs/roswell-emacs-config")))
